3769f40e3efdaf18f2b4485fc6a67fcc0992a3d1
Akash <akashgowda1802@gmail.com>
2025-07-08 13:59:46 +0530

    Clerk integration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,77 @@
 'use client'
 import { useState } from "react"
-import localFont from 'next/font/local';
-import { IoIosMenu } from "react-icons/io";
+import localFont from 'next/font/local'
+import { IoIosMenu } from "react-icons/io"
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const minecraftFont = localFont({
   src: "../../public/fonts/MinecraftBold-nMK1.otf"
 })
 
-export default function Header(){
-    const [isOpen, setIsOpen] = useState(false);
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleNavBar = () => setIsOpen(!isOpen)
 
-    const toggleNavBar = ()=>{
-        setIsOpen(!isOpen);
-    };  
+  return (
+    <header>
+      <div className="relative flex items-center justify-center mt-4">
+        <a className={`${minecraftFont.className} text-2xl select-none`} href="/">Laced Truth</a>
 
-    return(
-        <header>
-        <div className="relative flex items-center justify-center mt-4">
-        {/* <p className={`${minecraftFont.className} text-2xl select-none`}>Laced Truth</p> */}
-        <a className={`${minecraftFont.className} text-2xl select-none`}
-        href="/">Laced Truth</a>
-        <div className="hidden md:block absolute right-0 space-x-4 mr-4">
+        <div className="hidden md:flex absolute right-0 space-x-4 mr-4 items-center">
           <a href="/what" className="text-gray-400 hover:text-gray-200">What's this?</a>
           <a href="/about" className="text-gray-400 hover:text-gray-200">About</a>
-          <a href="/login" className="text-gray-400 hover:text-gray-200">Login</a>
-          <a href="/register" className="text-gray-400 hover:text-gray-200">Register</a>
-        </div>
-        <div className="md:hidden">
-            <button onClick={toggleNavBar} className="absolute right-0 top-2 mr-2">{isOpen ? 'X' : <IoIosMenu />}</button>
+          
+          <SignedOut>
+            <SignInButton>
+          <a className="text-gray-400 hover:text-gray-200 hover:cursor-pointer">Login</a>
+
+            </SignInButton>
+            <SignUpButton>
+          <a className="text-gray-400 hover:text-gray-200 hover:cursor-pointer">Register</a>
+              
+            </SignUpButton>
+          </SignedOut>
+
+          <SignedIn >
+            <UserButton showName appearance={{
+              variables:{
+                colorText: 'white'
+              }
+            }}/>
+          </SignedIn>
         </div>
 
-         {isOpen && (
-          <div className="md:hidden w-48 p-4 rounded-lg shadow-lg">
+        <div className="md:hidden">
+          <button onClick={toggleNavBar} className="absolute right-0 top-2 mr-2">
+            {isOpen ? 'X' : <IoIosMenu />}
+          </button>
+        </div>
+
+        {isOpen && (
+          <div className="md:hidden w-48 p-4 rounded-lg shadow-lg absolute right-2 top-12 bg-black">
             <a href="/what" className="block mb-2 hover:text-gray-200">What's this?</a>
             <a href="/about" className="block mb-2 hover:text-gray-200">About</a>
-            <a href="/login" className="block mb-2 hover:text-gray-200">Login</a>
-            <a href="/register" className="block mb-2 hover:text-gray-200">Register</a>
+            <SignedOut>
+              <SignInButton><a className="block mb-2 hover:text-gray-200 hover:cursor-pointer">Login</a></SignInButton>
+              <SignUpButton><a className="block mb-2 hover:text-gray-200 hover:cursor-pointer">Register</a></SignUpButton>
+            </SignedOut>
+
+            <SignedIn>
+              <UserButton showName appearance={{
+              variables:{
+                colorText: 'white'
+              }
+            }}/>
+            </SignedIn>
           </div>
         )}
-        
       </div>
-        </header>
-        
-    )
+    </header>
+  )
 }
